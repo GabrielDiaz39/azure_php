1334d4d3fd838803b5004c532ffba5a7868ea41f
GabrielDiaz39 <90275979+GabrielDiaz39@users.noreply.github.com>
2023-10-16 20:23:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Hello World";
+    $dbhost = "phpapp2323-server.postgres.database.azure.com";
+    $dbuser = "pqxgybivhf";
+    $dbpassword = "331JDVYE6PRJO7A7$";
+    $dbname = "phpapp2323-database";
+    $port = 5432;
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $port);
+    if (!$conn){
+        die("No hay conexión: ".mysqli_connect_error());
+    }
 ?>
